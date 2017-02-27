58c4fcc55f44a8dcc3ff6f2076a439d944573ea0
wan9cha0 <204073700@qq.com>
2017-02-27 11:42:30 +0800

    loader 2-27 11.42

--- js/common.js
+++ js/common.js
@@ -4,114 +4,123 @@
 
 
 */
-// head nav cont 头部内容
-let headNav = [{
-        icon: 'icon-alarm',
-        title: 'alarm'
-    },
-    {
-        icon: 'icon-audio',
-        title: 'audio'
-    },
-    {
-        icon: 'icon-contacts',
-        title: 'contacts'
-    },
-    {
-        icon: 'icon-share',
-        title: 'share'
-    }
-];
-let head = new Vue({
-    el: '#heads',
-    data: {
-        title: 'wishuice',
-        logo: '',
-        menu: false,
-        show: false,
-        nav: headNav,
-        headImg: 'img/head.jpeg'
-    },
-    methods: {
-        headNavHover: function() {
-            this.show = true
-        }
-    }
-})
+$(function(){
+  // head nav cont 头部内容
+  let headNav = [{
+          icon: 'icon-alarm',
+          title: 'alarm'
+      },
+      {
+          icon: 'icon-audio',
+          title: 'audio'
+      },
+      {
+          icon: 'icon-contacts',
+          title: 'contacts'
+      },
+      {
+          icon: 'icon-share',
+          title: 'share'
+      }
+  ];
+  let head = new Vue({
+      el: '#heads',
+      data: {
+          title: 'wishuice',
+          logo: '',
+          menu: false,
+          show: false,
+          nav: headNav,
+          headImg: 'img/head.jpeg'
+      },
+      methods: {
+          headNavHover: function() {
+              this.show = true
+          }
+      }
+  })
 
 
-// banner animation banner内容
+  // banner animation banner内容
 
-let banner = new Vue({
-    el: '#banner_box',
-    data: {
-        text: "A simple introduction to the English.A simple introduction to the English.A simple introduction to the English.A simple introduction to the English."
+  let banner = new Vue({
+      el: '#banner_box',
+      data: {
+          text: "A simple introduction to the English.A simple introduction to the English.A simple introduction to the English.A simple introduction to the English."
+      }
+  })
+
+  // skills lists 技能列表
+  let skills = new Vue({
+      el: '#skills',
+      data: {
+          name: 'zhegeshigesha',
+          skills: [{
+                  name: 'A',
+                  value: 90
+              },
+              {
+                  name: 'B',
+                  value: 80
+              },
+              {
+                  name: 'C',
+                  value: 95
+              },
+              {
+                  name: 'D',
+                  value: 70
+              },
+              {
+                  name: 'E',
+                  value: 75
+              },
+              {
+                  name: 'F',
+                  value: 85
+              },
+          ]
+      }
+  })
+  // works
+  let works = new Vue({
+    el:'#works',
+    data:{
+      text:'This is a list of works,This is a list of works,This is a list of works',
+      img:[
+        {url:'img/works/works01.png',size:'big'},
+        {url:'img/works/works04.png',size:'middle'},
+        {url:'img/works/works02.png',size:'big'},
+        {url:'img/works/works09.png',size:'small'},
+        {url:'img/works/works05.png',size:'middle'},
+        {url:'img/works/works03.png',size:'big'},
+        {url:'img/works/works07.png',size:'middle'},
+        {url:'img/works/works06.png',size:'middle'},
+        {url:'img/works/works08.png',size:'small'},
+      ]
     }
+  })
+  // footer
+  let footer = new Vue({
+    el:'#foot',
+    data:{
+      text:'2017 wan9cha0'
+    }
+  })
 })
 
-// skills lists 技能列表
-let skills = new Vue({
-    el: '#skills',
-    data: {
-        name: 'zhegeshigesha',
-        skills: [{
-                name: 'A',
-                value: 90
-            },
-            {
-                name: 'B',
-                value: 80
-            },
-            {
-                name: 'C',
-                value: 95
-            },
-            {
-                name: 'D',
-                value: 70
-            },
-            {
-                name: 'E',
-                value: 75
-            },
-            {
-                name: 'F',
-                value: 85
-            },
-        ]
-    }
-})
-// works
-let works = new Vue({
-  el:'#works',
-  data:{
-    text:'This is a list of works,This is a list of works,This is a list of works',
-    img:[
-      {url:'img/works/works01.png',size:'big'},
-      {url:'img/works/works04.png',size:'middle'},
-      {url:'img/works/works02.png',size:'big'},
-      {url:'img/works/works09.png',size:'small'},
-      {url:'img/works/works05.png',size:'middle'},
-      {url:'img/works/works03.png',size:'big'},
-      {url:'img/works/works07.png',size:'middle'},
-      {url:'img/works/works06.png',size:'middle'},
-      {url:'img/works/works08.png',size:'small'},
-    ]
-  }
-})
-// footer
-let footer = new Vue({
-  el:'#foot',
-  data:{
-    text:'2017 wan9cha0'
-  }
-})
 // 页面动画
-let bannerImg = $('#banner_box');
-let changHead = $('#skills');
-let backHead = $('#restore-head');
-let worksHead = $('.works-list');
+
 $(document).ready(function() {
+  setTimeout(function(){
+    $('.loader-index').hide();
+  },2000);
+
+  let bannerImg = $('#banner_box');
+  let changHead = $('#skills');
+  let backHead = $('#restore-head');
+  let worksHead = $('.works-list');
+
     bannerImg.height($(window).height());
     let before = $(window).scrollTop();
     $(window).scroll(function() {
@@ -146,3 +155,30 @@ $(document).ready(function() {
         $('.banner-in').css('transform', 'translateY(' + (-.5 * before) + 'px)');
     })
 })
+
+
+// // chart
+// var myChart option = {
+//     title: {
+//         text: '水波晃动的效果'
+//     },
+//     series: [{
+//         type: 'liquidFill',
+//         data: [0.9, {
+//             value: 0.9,
+//             phase: Math.PI
+//         }],
+//         phase: 0,
+//         period: 4000,
+//         waveLength: '100%',
+//         animationDurationUpdate: 2000
+//     }]
+// };
+//
+// setInterval(function() {
+//     myChart.setOption({
+//         series: [{
+//             amplitude: Math.random() * 40
+//         }]
+//     })
+// }, 2000);
